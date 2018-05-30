34a20cdd16c12c0f743382fd7a94f3fec32754f3
Andrea Law <abylaw@gmail.com>
2018-05-29 22:15:04 -0400

    Update error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <section class='section' :style="{
+    <max-width :style="{
+      backgroundColor: 'white',
       textAlign: 'center',
+      padding: '2em 0',
     }">
-      <h1>404</h1>
-      <h3>Oops. The page you were looking for doesn't exist.</h3>
-      <p>You may have mistyped the address or the page may have moved</p>
+      <div v-if="error.statusCode === 404">
+        <h1>404</h1>
+        <h3>Oops. The page you were looking for doesn't exist.</h3>
+        <p>You may have mistyped the address or the page may have moved</p>
+      </div>
+
+      <div v-else>
+        <h1>An error occurred</h1>
+      </div>
 
       <nuxt-link to='/'><button class='secondary'>Go to homepage</button></nuxt-link>
-    </section>
+    </max-width>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
