b9d55fa31775e210cd76d9027c82f545c45694ab
Jean Barriere <jean@barriere.io>
2018-11-06 13:20:45 +0100

    feat(glorious): add glorious demo component and replaced the previous demo by that component on the storyscript section

--- src/views/Home/Demo.vue
+++ src/views/Home/Demo.vue
@@ -7,86 +7,17 @@
           <p>Meet Storyscript, the first goal-oriented<br>programming language, empowering<br>developers to focus on features; deploying in<br>a fraction of the time, scaling in days rather<br>than months.</p>
         </div>
         <div class="column is-half">
-          <a-window
+          <a-demo :actions="actions"/>
+          <!-- <a-window
             class="story"
             title="http.story">{{ code }}</a-window>
           <p>Horizontal auto-scaling</p>
           <a-window
             class="story"
             title="slack.story">{{ code2 }}</a-window>
-          <p>Create integrations in minutes, deploying in seconds</p>
+          <p>Create integrations in minutes, deploying in seconds</p> -->
         </div>
       </div>
-      <!-- <a-tabs class="container">
-        <a-card light>
-          <a-tab-pane title="Serverless HTTP">
-            <div class="columns">
-              <div class="column is-three-fifths">
-                <a-window>{{ code }}</a-window>
-                <a-window
-                  :copy="false"
-                  type="terminal"
-                  lang="bash">{{ deploy }}</a-window>
-              </div>
-              <div class="column is-two-fifths">
-                <a-window
-                  title="wonderful-app-666.asyncyapp.com"
-                  type="browser">
-                  <h3>hello world!</h3>
-                </a-window>
-              </div>
-            </div>
-          </a-tab-pane>
-          <a-tab-pane title="Slack Bot">
-            <div class="columns">
-              <div class="column is-three-fifths">
-                <a-window title="Source">{{ code2 }}</a-window>
-                <a-window
-                  :copy="false"
-                  title="Logs"
-                  type="terminal"
-                  lang="bash">{{ deploy2 }}</a-window>
-              </div>
-              <div class="column is-two-fifths">
-                <a-window
-                  title="mycompany.slack.com"
-                  type="browser">
-                  <div class="columns is-mobile is-gapless">
-                    <div class="column is-one-fifth bg--dark">
-                      <p>hello</p>
-                    </div>
-                    <div class="column is-one-fifth bg--light">
-                      <p>channel</p>
-                    </div>
-                    <div class="column is-three-fifths bg--white">
-                      <p>content</p>
-                    </div>
-                  </div>
-                </a-window>
-              </div>
-            </div>
-          </a-tab-pane>
-          <a-tab-pane title="Machine Learning">
-            <div class="columns stretch">
-              <div class="column is-three-fifths">
-                <a-window title="Source">{{ code3 }}</a-window>
-                <a-window
-                  :copy="false"
-                  title="Logs"
-                  type="terminal"
-                  lang="bash">{{ deploy3 }}</a-window>
-              </div>
-              <div class="column is-two-fifths">
-                <a-window
-                  title="cat-or-dog.org"
-                  type="browser">
-                  <h3>Upload your picture</h3>
-                </a-window>
-              </div>
-            </div>
-          </a-tab-pane>
-        </a-card>
-      </a-tabs> -->
     </div>
   </div>
 </template>
@@ -95,11 +26,32 @@
 export default {
   name: 'Demo',
   data: () => ({
-    code: `http server as server\n  when server listen path:'/' method:'get' as req\n    req write content:'hello world!'`,
-    code2: `slack bot as bot\n  when bot hears pattern:/weather/ as message\n    location = corenlp location text:message.text\n    fc = accuweather forecast location:location\n    slack reply thread_ts:message.thread_ts text:fc`,
+    actions: [{
+      type: 'editor',
+      minHeight: '554px',
+      code: `// Horizontal auto-scaling\n\nhttp server as server\n  when server listen path:'/' method:'get' as req\n    req write content:'hello world!'\n`,
+      windowTitle: 'app.story'
+    }, {
+      type: 'editor',
+      code: `// Create integrations in minutes, deploying in seconds\n\nslack bot as bot\n  when bot hears pattern:/weather/ as message\n    location = corenlp location text:message.text\n    fc = accuweather forecast location:location\n    slack reply thread_ts:message.thread_ts text:fc\n`
+    }, {
+      type: 'terminal',
+      minHeight: '554px',
+      command: 'cat ./app.story',
+      respond: `// Horizontal auto-scaling\n\nhttp server as server\n  when server listen path:'/' method:'get' as req\n    req write content:'hello world!'\n// Create integrations in minutes, deploying in seconds\n\nslack bot as bot\n  when bot hears pattern:/weather/ as message\n    location = corenlp location text:message.text\n    fc = accuweather forecast location:location\n    slack reply thread_ts:message.thread_ts text:fc`
+    }, {
+      type: 'terminal',
+      command: 'asyncy deploy',
+      respond: '√ Deployed!\nIf your story listens to HTTP requests, visit https://my-project.asyncyapp.com'
+    }, {
+      type: 'terminal',
+      command: 'curl my-project.asyncyapp.com',
+      respond: 'hello world!'
+    }, {
+      type: 'terminal',
+      command: ''
+    }],
     code3: `Need to be written`,
-    deploy: `$> asyncy deploy\n√ Deployed!\nIf your story listens to HTTP requests, visit https://wonderful-app-666.asyncyapp.com`,
-    deploy2: `$> asyncy deploy\n√ Deployed!\nIf your story listens to HTTP requests, visit https://xxx-yyy-777.asyncyapp.com`,
     deploy3: `$> asyncy deploy\n√ Deployed!\nIf your story listens to HTTP requests, visit https://cat-or-dog.org`
   }),
   methods: {
@@ -112,6 +64,6 @@ export default {
 
 <style lang="scss" scoped>
 .story {
-  margin-top: 1rem;
+  // margin-top: 1rem;
 }
 </style>
