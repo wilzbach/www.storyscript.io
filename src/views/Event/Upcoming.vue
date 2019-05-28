c36ef10f66151f4ef80012d0926d6e5e1c07b0ed
Jean Barriere <jean@barriere.io>
2019-05-28 16:56:40 +0200

    feat(events): update upcoming events

--- src/views/Event/Upcoming.vue
+++ src/views/Event/Upcoming.vue
@@ -24,21 +24,16 @@ export default {
   name: 'Upcoming',
   components: { CardCalendar },
   data: () => ({
-    events: [ {
-      title: 'OSS Leadership',
-      date: new Date('2019-03-12 00:00:00'),
-      picture: '//events.linuxfoundation.org/wp-content/uploads/2018/08/39764015295_3d0ddb5950_o.jpg',
-      link: '//events.linuxfoundation.org/events/open-source-leadership-summit-2019/register/'
+    events: [{
+      title: 'GOTO Amsterdam',
+      date: new Date('2019-06-17 00:00:00'),
+      picture: '//files.gotocon.com/uploads/images/conference_11/pages/173/original/front_of_the_beurs_van_berlage_damrak_at_night1.jpg',
+      link: '//gotoams.nl'
     }, {
-      title: 'The next Web',
-      date: new Date('2019-05-09 00:00:00'),
-      picture: '//s3.amazonaws.com/events.tnw/hardfork-2018/uploads/visuals/NDSM_tinypng2.png',
-      link: '//thenextweb.com/conference/'
-    }, {
-      title: 'Kubecon EU',
-      date: new Date('2019-05-20 00:00:00'),
-      picture: '//events.linuxfoundation.org/wp-content/uploads/2018/08/Barcelona.jpg',
-      link: '//events.linuxfoundation.org/events/kubecon-cloudnativecon-europe-2019/'
+      title: 'GitHub Universe',
+      date: new Date('2019-11-13 00:00:00'),
+      picture: '//event-sponsorship.github.com/assets/images/events/universe/universe-modal-1.jpg',
+      link: '//githubuniverse.com'
     }]
   })
 }
